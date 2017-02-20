9887a9cf87b8afc9c651e43cc60f3ab367b65be4
kevin <kevin>
2017-02-19 19:23:01 -0800

    runnable

---
+++ A01/Assignment1.java
@@ -0,0 +1,19 @@
+class Matrix {
+  class Worker implements Runnable {
+    Worker() {
+    
+    }
+    public void run() {
+
+    }
+  }
+  private double[][] M;
+  
+  
+}
+
+class Assignment1 {
+  public static void main(String args[]){
+    System.out.println("Hello");
+  }
+}
--- A01/MultiThreadedMatrix.java
+++ A01/MultiThreadedMatrix.java
@@ -1,3 +1,7 @@
+public class Matrix {
+    
+}
+
 public class MultiThreadedMatrix {
     public static void main(String args[]){
         System.out.println("Hello");
